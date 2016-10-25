6f525aef7a55b6ba54149ca0876bd9478710e01f
megganeturner <meggan.turner@hotmail.com>
2016-10-26 09:46:08 +1100

    user mv*

---
+++ app/assets/javascripts/views/UserView.js
@@ -0,0 +1,10 @@
+var app = app || {};
+
+app.UserView = Backbone.view.extend({
+  tagName : 'li',
+  render: function(){
+    var name = this.model.get('name');
+    this.$el.text(name);
+    $('#viewUsers').prepend(this.$el);
+  }
+})
