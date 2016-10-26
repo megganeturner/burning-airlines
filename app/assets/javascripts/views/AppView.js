67624c622f8a231fe3a4f7f5334d642ed121fdc3
Cedric Roux <cedricroux@Cedrics-MacBook-Air.local>
2016-10-26 15:30:27 +1100

    ba

--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -3,7 +3,12 @@ var app = app || {};
 app.AppView = Backbone.View.extend({
   el: '#main',
   render: function () {
-    this.$el.html( $('#appView').html() );
+
+    var appViewTemplate = $("#appViewTemplate").html();
+    this.$el.html(appViewTemplate);
+
+    var searchInputView = new app.searchInputView();
+    searchInputView.render();
 
   }
-})
+});
---
+++ app/assets/javascripts/views/FlightListView.js
@@ -0,0 +1,16 @@
+var app = app || {};
+
+app.FlightListView = Backbone.View.extend({
+  tagName: 'li', //Create a new element for each instance of this view.
+  events: {
+    'click': 'showFlight',
+  },
+  showFlight: function () {
+    app.router.navigate('flights/' + this.model.get('id'), true);
+  },
+  render: function () {
+    this.$el.text( this.model.get('flight_num'));
+    this.$el.appendTo('#flightLists');
+
+  }
+})
