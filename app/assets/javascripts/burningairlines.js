427d7ac491d1f789509d52ba7ead89a1def263af
Cedric Roux <cedricroux@Cedrics-Air.hotspot>
2016-10-26 23:56:39 +1100

    search fix

--- app/assets/javascripts/burningairlines.js
+++ app/assets/javascripts/burningairlines.js
@@ -12,24 +12,20 @@ $(document).ready(function () {
   app.router = new app.AppRouter();
   Backbone.history.start();
 
-<<<<<<< HEAD
 })
-=======
 
-
-app.BurningFlights = new app.Flights;
-
-$(document).ready(function(){
-  if ($('#main').length === 0) {
-    return;
-  }
-
-  app.router = new app.Router();
-
-  var app_view = new AppView({ el: $("#main") });
-
-  flights.fetch().done(function(){
-    Backbone.history.start();
-  });
-});
->>>>>>> 3e67f2bbb2ea7afab8f2bdf5a9608449e896260c
+// app.BurningFlights = new app.Flights;
+//
+// $(document).ready(function(){
+//   if ($('#main').length === 0) {
+//     return;
+//   }
+//
+//   app.router = new app.Router();
+//
+//   var app_view = new AppView({ el: $("#main") });
+//
+//   flights.fetch().done(function(){
+//     Backbone.history.start();
+//   });
+// });
